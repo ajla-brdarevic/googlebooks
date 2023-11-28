ca80d5ba9af811714d2a14ec04b6d03c13d8e312
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-11-28 17:08:30 +0100

    update search input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
+      );
+
+      setSearchResults(response.data.items);
+    } catch (error) {
+      console.error('Error fetching data from Google Books API:', error);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
-    <div className="App">
-      <div className="search">
-        <input type='text' placeholder='Enter Book Name'></input>
+    <div className='app'>
+      <div className="header">
+        <div className="search">
+          <input type='text' placeholder='Search book' value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handleKeyDown}
+          />
+        </div>
+      </div>
+
+        <div className="search-results">
+        {searchResults.map((book) => (
+          <div key={book.id}>{book.volumeInfo.title}</div>
+        ))}
       </div>
     </div>
   );
