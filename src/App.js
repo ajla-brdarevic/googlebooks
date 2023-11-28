c6321fc86250012cfc6deb77e75697fae7a91656
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-11-28 18:07:39 +0100

    update random books on main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [randomBooks, setRandomBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchRandomBooks = async () => {
+      try {
+        const response = await axios.get(
+          'https://www.googleapis.com/books/v1/volumes?q=book&maxResults=18'
+        );
+
+        const shuffledResults = response.data.items.sort(() => 0.5 - Math.random()).slice(0, 18);
+
+        setRandomBooks(shuffledResults);
+      } catch (error) {
+        console.error('Error fetching random books:', error);
+      }
+    };
+
+    fetchRandomBooks();
+  }, []);
 
   const handleSearch = async () => {
     try {
@@ -38,6 +58,18 @@ const App = () => {
       </div>
 
       <div className="container">
+        {searchResults.length === 0 && randomBooks.map((book) => (
+          <div key={book.id} className='book-info'>
+            <img
+              src={book.volumeInfo.imageLinks?.thumbnail || ''}
+              alt={book.volumeInfo.title}
+              className='book-cover'
+            />
+            <h3>{book.volumeInfo.title}</h3>
+            <h5>{book.volumeInfo.authors}</h5>
+          </div>
+        ))}
+
         {searchResults.map((book) => (
           <div key={book.id} className='book-info'>
             <img
